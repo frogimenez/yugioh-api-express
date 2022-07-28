9993776ef1f7a60819a2223178664d567e26d1f6
Fernando <frogimenez@gmail.com>
2022-07-28 22:39:22 +0200

    perf: :zap: improve get

--- index.js
+++ index.js
@@ -3,34 +3,38 @@ const app = express();
 
 let cards = [
     {
-    name: "Dragon Blanco de tres cabezas",
-    ATK: 4500,
-    DEF: 2000,
-    Nivel: 8,
-    Atributo: "Luz",
+    "id": 1,
+    "name": "Dragon Blanco de tres cabezas",
+    "ATK": 4500,
+    "DEF": 2000,
+    "Nivel": 8,
+    "Atributo": "Luz",
     },
     {
-    name: "Mago Negro",
-    ATK: 4000,
-    DEF: 4000,
-    Nivel: 8,
-    Atributo: "Oscuridad",
+    "id": 2,
+    "name": "Mago Negro",
+    "ATK": 4000,
+    "DEF": 4000,
+    "Nivel": 8,
+    "Atributo": "Oscuridad",
     },
     {
-    name: "Ala Gris",
-    ATK: 3000,
-    DEF: 1500,
-    Nivel: 6,
-    Atributo: "Viento",
+    "id": 3,
+    "name": "Ala Gris",
+    "ATK": 3000,
+    "DEF": 1500,
+    "Nivel": 6,
+    "Atributo": "Viento",
     },
     {
-    name: "Albion el Dragon Señalado",
-    ATK: 2000,
-    DEF: 2000,
-    Nivel: 4,
-    Atributo: "Fuego",
+    "id": 4,
+    "name": "Albion el Dragon Señalado",
+    "ATK": 2000,
+    "DEF": 2000,
+    "Nivel": 4,
+    "Atributo": "Fuego",
     },
-];
+]
 
 // const app = http.createServer((request,response)=>{
 //     response.writeHead(200,{'Content-type': 'application/json'})
@@ -43,6 +47,14 @@ app.get("/", (req, resp) => {
 
 app.get('/api/cards', (req,resp) =>{
     resp.json(cards)
+})
+
+app.get('/api/cards/:id', (req,resp) =>{
+    const id = req.params.id
+    console.log({cards});
+    console.log({id});
+    const card = cards.find( card => card.id === id)
+    resp.json(card)
 })
 
 const PORT = 3000;
