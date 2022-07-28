ab0e6c37b4f19a448d83e4c253e41a87762ec500
Fernando <frogimenez@gmail.com>
2022-07-28 22:27:11 +0200

    build: :zap: small improvement with espress

--- index.js
+++ index.js
@@ -1,42 +1,52 @@
-const http = require('http')
+const express = require("express");
+const app = express();
 
 let cards = [
     {
-        "name":"Dragon Blanco de tres cabezas",
-        "ATK": 4500,
-        "DEF": 2000,
-        "Nivel": 8,
-        "Atributo": "Luz"
+    name: "Dragon Blanco de tres cabezas",
+    ATK: 4500,
+    DEF: 2000,
+    Nivel: 8,
+    Atributo: "Luz",
     },
     {
-        "name":"Mago Negro",
-        "ATK": 4000,
-        "DEF": 4000,
-        "Nivel": 8,
-        "Atributo": "Oscuridad"
+    name: "Mago Negro",
+    ATK: 4000,
+    DEF: 4000,
+    Nivel: 8,
+    Atributo: "Oscuridad",
     },
     {
-        "name":"Ala Gris",
-        "ATK": 3000,
-        "DEF": 1500,
-        "Nivel": 6,
-        "Atributo": "Viento"
+    name: "Ala Gris",
+    ATK: 3000,
+    DEF: 1500,
+    Nivel: 6,
+    Atributo: "Viento",
     },
     {
-        "name":"Albion el Dragon Señalado",
-        "ATK": 2000,
-        "DEF": 2000,
-        "Nivel": 4,
-        "Atributo": "Fuego"
+    name: "Albion el Dragon Señalado",
+    ATK: 2000,
+    DEF: 2000,
+    Nivel: 4,
+    Atributo: "Fuego",
     },
-]
+];
 
+// const app = http.createServer((request,response)=>{
+//     response.writeHead(200,{'Content-type': 'application/json'})
+//     response.end(JSON.stringify(cards))
+// })
 
-const app = http.createServer((request,response)=>{
-    response.writeHead(200,{'Content-type': 'application/json'})
-    response.end(JSON.stringify(cards))
+app.get("/", (req, resp) => {
+    resp.send('<h1>Hello World</h1>')
 })
 
-const PORT = 3000
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+app.get('/api/cards', (req,resp) =>{
+    resp.json(cards)
+})
+
+const PORT = 3000;
+app.listen(PORT,() => {
+    console.log(`Server running on port ${PORT}`)
+})
+
