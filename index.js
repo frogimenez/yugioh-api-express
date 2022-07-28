8faf3452398f61a341e481c8e66e33340fa406cb
Fernando <frogimenez@gmail.com>
2022-07-28 21:10:17 +0200

    build: :construction: build package and server nodemon

--- index.js
+++ index.js
@@ -1,10 +1,42 @@
 const http = require('http')
 
+let cards = [
+    {
+        "name":"Dragon Blanco de tres cabezas",
+        "ATK": 4500,
+        "DEF": 2000,
+        "Nivel": 8,
+        "Atributo": "Luz"
+    },
+    {
+        "name":"Mago Negro",
+        "ATK": 4000,
+        "DEF": 4000,
+        "Nivel": 8,
+        "Atributo": "Oscuridad"
+    },
+    {
+        "name":"Ala Gris",
+        "ATK": 3000,
+        "DEF": 1500,
+        "Nivel": 6,
+        "Atributo": "Viento"
+    },
+    {
+        "name":"Albion el Dragon Señalado",
+        "ATK": 2000,
+        "DEF": 2000,
+        "Nivel": 4,
+        "Atributo": "Fuego"
+    },
+]
+
+
 const app = http.createServer((request,response)=>{
-    response.writeHead(200,{'Content-type': 'text-plain'})
-    response.end('Hello Worls')
+    response.writeHead(200,{'Content-type': 'application/json'})
+    response.end(JSON.stringify(cards))
 })
 
-const PORT = 3001
+const PORT = 3000
 app.listen(PORT)
 console.log(`Server running on port ${PORT}`)
